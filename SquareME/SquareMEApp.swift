ef096d6a70266c60b16d688530cb210e874f7755
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-02-02 14:04:22 -0500

    Initial Commit

---
+++ SquareME/SquareMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquareMEApp.swift
+//  SquareME
+//
+//  Created by Graeme Armstrong on 2023-02-02.
+//
+
+import SwiftUI
+
+@main
+struct SquareMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
